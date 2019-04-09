06ab9a1377161ad99e00c99aebce404bc2dc8cd4
Rob Bronson <robert.j.bronson+cf@gmail.com>
2019-04-08 21:30:08 -0700

    basic functionality working.  Still need to render results and commentary

---
+++ js/app.js
@@ -0,0 +1,103 @@
+'use strict';
+
+// Data declarations
+let timesClicked = 0;
+let products = [];
+let previousNums = [];
+//array for image data [ [imageName, source], ..... ]
+let images = [
+  ['bag', './img/bag.jpg'],
+  ['banana', './img/banana.jpg'],
+  ['bathroom', './img/bathroom.jpg'],
+  ['boots', './img/boots.jpg'],
+  ['breakfast', './img/breakfast.jpg'],
+  ['bubblegum', './img/bubblegum.jpg'],
+  ['chair', './img/chair.jpg'],
+  ['cthulhu', './img/cthulhu.jpg'],
+  ['dog-duck', './img/dog-duck.jpg'],
+  ['dragon', './img/dragon.jpg'],
+  ['pen', './img/pen.jpg'],
+  ['pet-sweep', './img/pet-sweep.jpg'],
+  ['scissors', './img/scissors.jpg'],
+  ['shark', './img/shark.jpg'],
+  ['sweep', './img/sweep.png'],
+  ['tauntaun', './img/tauntaun.jpg'],
+  ['unicorn', './img/unicorn.jpg'],
+  ['usb', './img/usb.gif'],
+  ['water-can', './img/water-can.jpg'],
+  ['wine-glass', './img/wine-glass.jpg']
+];
+
+let imgIds = ['one', 'two', 'three'];
+
+// Behaviors
+//constructor for image objects
+function ImageObject(image) {
+  this.name = image[0];
+  this.src = image[1];
+  this.viewed = 0;
+  this.clicked = 0;
+
+  products.push(this);
+}
+
+images.forEach(value => new ImageObject(value));
+// console.log(products);
+
+//function to place images in DOM
+function getImageIndices() {
+  //new set of numbers
+  console.log('previous Nums ', previousNums);
+  let newNums = [];
+  while (newNums.length < 3) {
+    let num = Math.floor(Math.random() * 20);
+    console.log('temp num ', num);
+    if (previousNums.includes(num) || newNums.includes(num)) {
+      continue;
+    } else {
+      newNums.push(num);
+    }
+  }
+  console.log('new nums ', newNums);
+  previousNums = newNums;
+  return newNums;
+}
+
+function swapImages() {
+  let nums = getImageIndices();
+  for (let i = 0; i < imgIds.length; i++) {
+    let el = document.getElementById(imgIds[i]);
+    el.alt = products[nums[i]].name;
+    el.src = products[nums[i]].src;
+    products[nums[i]].viewed += 1;
+  }
+  console.log(previousNums);
+}
+
+function countClicks(event) {
+  if (event.target.id === 'one') {
+    products[previousNums[0]].clicked += 1;
+  } else if (event.target.id === 'two') {
+    products[previousNums[1]].clicked += 1;
+  } else if (event.target.id === 'three') {
+    products[previousNums[2]].clicked += 1;
+  }
+}
+
+// Execution
+swapImages(); //initiate DOM with images.
+
+let targetEl = document.getElementById('images');
+
+targetEl.addEventListener('click', function(event) {
+  console.log(event.target);
+  countClicks(event);
+  swapImages();
+  timesClicked++;
+  if (timesClicked >= 25) {
+    let elImages = document.getElementById('images');
+    let elResults = document.getElementById('results');
+    elImages.style.display = 'none';
+    elResults.style.display = 'block';
+  }
+});
